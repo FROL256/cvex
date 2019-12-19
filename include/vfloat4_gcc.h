010afc01025d0616921ac22a29df8cc0a72aa05d
vfrolov <vova@frolov.pp.ru>
2019-12-19 22:03:47 +0300

    fix gcc ver

--- include/vfloat4_gcc.h
+++ include/vfloat4_gcc.h
@@ -55,16 +55,9 @@ namespace cvex
   static inline vuint4  splat(_uint32_t x) { return vuint4 {x,x,x,x}; }
   static inline vfloat4 splat(float x)     { return vfloat4{x,x,x,x}; }
 
-  // convert and cast
-  //
-  static inline vint4   to_int32  (vfloat4 a)  { return vint4  { (int)a[0], (int)a[1], (int)a[2], (int)a[3]}; }
-  static inline vint4   to_int32  (vuint4  a)  { return (vint4)a; }
-
-  static inline vuint4  to_uint32 (vfloat4 a)  { return vuint4 { (_uint32_t)a[0], (_uint32_t)a[1], (_uint32_t)a[2], (_uint32_t)a[3]}; }
-  static inline vuint4  to_uint32 (vint4   a)  { return (vuint4)a; }
   
-  static inline vfloat4 to_float32(vint4 a)    { return vfloat4{(float)a[0], (float)a[1], (float)a[2], (float)a[3]}; }
-  static inline vfloat4 to_float32(vuint4 a)   { return vfloat4{(float)a[0], (float)a[1], (float)a[2], (float)a[3]}; }
+  static inline vfloat4 to_float32(const vint4 a)    { return vfloat4{(float)a[0], (float)a[1], (float)a[2], (float)a[3]}; }
+  static inline vfloat4 to_float32(const vuint4 a)   { return vfloat4{(float)a[0], (float)a[1], (float)a[2], (float)a[3]}; }
   
   static inline vfloat4 as_float32(const vint4 a_val)   { return reinterpret_cast<vfloat4>(a_val); }
   static inline vfloat4 as_float32(const vuint4 a_val)  { return reinterpret_cast<vfloat4>(a_val); }
@@ -91,6 +84,14 @@ namespace cvex
   static inline vuint4 max  (const vuint4 a, const vuint4 b) { return a > b ? a : b; }
   static inline vuint4 clamp(const vuint4 x, const vuint4 minVal, const vuint4 maxVal) { return max(min(x, maxVal), minVal); }
 
+  // convert and cast
+  //
+  static inline vint4   to_int32(const vfloat4 a)  { return vint4  { (int)a[0], (int)a[1], (int)a[2], (int)a[3]}; }
+  static inline vint4   to_int32(const vuint4  a)  { return (vint4)a; }
+
+  static inline vuint4  to_uint32(const vint4   a)     { return (vuint4)a; }
+  static inline vuint4  to_uint32(const vfloat4 a_val) { return (vuint4)(max(to_int32(a_val),vint4{0,0,0,0})); }
+
   // shuffle operations ...
   //
   static inline vint4   blend(const vint4 a,   const vint4 b,   const vint4  mask) { return ((mask & a) | (~mask & b)); }
@@ -111,36 +112,49 @@ namespace cvex
 
   static inline bool test_bits_any(const vint4 a)
   {
-    //return (a[0] != 0 && a[1] != 0 && a[2] != 0 && a[3] != 0);
     const _sint64_t a2 = ( *(_sint64_t*)&a ) | ( *(_sint64_t*)&a[2] );
     return (a2 != 0);
   }
 
-  static inline bool test_bits_any(const vuint4 a)
+  static inline bool test_bits_all(const vint4 a)
   {
-    //return (a[0] != 0 && a[1] != 0 && a[2] != 0 && a[3] != 0);
-    const _uint64_t a2 = ( *(_uint64_t*)&a ) | ( *(_uint64_t*)&a[2] );
-    return (a2 != 0);
+    const _uint64_t* p1 = (const _uint64_t*)&a;
+    const _uint64_t* p2 = p1 + 1;
+    const _uint64_t a2 = (*p1) & (*p2);
+    return (a2 == _uint64_t(0xFFFFFFFFFFFFFFFF));
   }
 
-  static inline bool test_bits_any(const vfloat4 a) { return test_bits_any(reinterpret_cast<vuint4>(a)); }
-  
-  // test_bits_all (!!!)
-
-  #ifdef __x86_64
-  static void set_ftz() { _MM_SET_ROUNDING_MODE(_MM_ROUND_TOWARD_ZERO); }
-  #else
-  static void set_ftz() {}
-  #endif
+  static inline bool test_bits_any(const vuint4 a)  { return test_bits_any(reinterpret_cast<vint4>(a)); }
+  static inline bool test_bits_any(const vfloat4 a) { return test_bits_any(reinterpret_cast<vint4>(a)); }
+  static inline bool test_bits_all(const vuint4 a)  { return test_bits_all(reinterpret_cast<vint4>(a)); }
+  static inline bool test_bits_all(const vfloat4 a) { return test_bits_all(reinterpret_cast<vint4>(a)); }
 
   static inline void prefetch(const float* ptr) {  __builtin_prefetch(ptr); }
   static inline void prefetch(const int* ptr)   {  __builtin_prefetch(ptr); }
+
+  static inline vfloat4 shuffle_zyxw(vfloat4 a_src) { return __builtin_shuffle(a_src, vint4{2,1,0,3}); }
+  static inline vfloat4 shuffle_yzxw(vfloat4 a_src) { return __builtin_shuffle(a_src, vint4{1,2,0,3}); }
+  static inline vfloat4 shuffle_zxyw(vfloat4 a_src) { return __builtin_shuffle(a_src, vint4{2,0,1,3}); }
+
+  static inline vfloat4 shuffle_xyxy(vfloat4 a_src) { return __builtin_shuffle(a_src, vint4{0,1,0,1});  }
+  static inline vfloat4 shuffle_zwzw(vfloat4 a_src) { return __builtin_shuffle(a_src, vint4{2,3,2,3}); }
+
+  static inline vfloat4 cross3(const vfloat4 a, const vfloat4 b) 
+  {
+    const vfloat4 a_yzx = shuffle_yzxw(a);
+    const vfloat4 b_yzx = shuffle_yzxw(b);
+    return shuffle_yzxw(a*b_yzx - a_yzx*b);
+  }
 
   #ifdef __x86_64
   static inline float   dot3f(const vfloat4 a, const vfloat4 b) { return _mm_cvtss_f32(_mm_dp_ps(a, b, 0x7f)); }
   static inline vfloat4 dot3v(const vfloat4 a, const vfloat4 b) { return _mm_dp_ps(a, b, 0x7f); }
   static inline vfloat4 dot4v(const vfloat4 a, const vfloat4 b) { return _mm_dp_ps(a, b, 0xff); }
   static inline float   dot4f(const vfloat4 a, const vfloat4 b) { return _mm_cvtss_f32(_mm_dp_ps(a, b, 0xff)); }
+
+  static inline vfloat4 floor(const vfloat4 a_val) { return _mm_floor_ps(a_val); }
+  static inline vfloat4 ceil(const vfloat4 a_val)  { return _mm_ceil_ps(a_val);  }
+  static inline void set_ftz() { _MM_SET_ROUNDING_MODE(_MM_ROUND_TOWARD_ZERO); }
   #else
   static inline float   dot3f(const vfloat4 a, const vfloat4 b) 
   {
@@ -168,31 +182,24 @@ namespace cvex
     return vfloat4{res,res,res,res}; 
   }
   
-  static inline vfloat4 shuffle_zyxw(vfloat4 a_src) { return __builtin_shuffle(a_src, vint4{2,1,0,3}); }
-  static inline vfloat4 shuffle_yzxw(vfloat4 a_src) { return __builtin_shuffle(a_src, vint4{1,2,0,3}); }
-  static inline vfloat4 shuffle_zxyw(vfloat4 a_src) { return __builtin_shuffle(a_src, vint4{2,0,1,3}); }
-
-  static inline vfloat4 shuffle_xyxy(vfloat4 a_src) { return __builtin_shuffle(a_src, vint4{0,1,0,1});  }
-  static inline vfloat4 shuffle_zwzw(vfloat4 a_src) { return __builtin_shuffle(a_src, vint4{2,3,2,3}); }
-
-  static inline vfloat4 cross3(const vfloat4 a, const vfloat4 b) 
+  static inline vfloat4 ceil(const vfloat4 a)
   {
-    const vfloat4 a_yzx = shuffle_yzxw(a);
-    const vfloat4 b_yzx = shuffle_yzxw(b);
-    return shuffle_yzxw(a*b_yzx - a_yzx*b);
+    const vfloat4 res = {::ceil(a[0]), ::ceil(a[1]), ::ceil(a[2]), ::ceil(a[3])};
+    return res;
   }
-  
+
+  static inline vfloat4 floor(const vfloat4 a)
+  {
+    const vfloat4 res = {::floor(a[0]), ::floor(a[1]), ::floor(a[2]), ::floor(a[3])};
+    return res;
+  }
+
+  inline static void set_ftz() {}
   #endif
 
-  // cross3
   // length3
   // cmpgt3
-
   // extract_<0,1,2,3>
-
-  // shuffle2_xy_xy, shuffle2_xy_zw, shuffle2_zw_zw ?
-  
-  // add_s, subb_s, ... ?
 
   //color_compress_bgra, color_compress_rgba 
 
