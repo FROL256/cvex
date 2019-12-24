709212d3aad6b79f5a999fb64aca4c608149bed1
vfrolov <vova@frolov.pp.ru>
2019-12-24 15:35:01 +0300

    edit

--- include/cmath.h
+++ include/cmath.h
@@ -31,26 +31,16 @@ namespace cmath
 
   struct float4
   {
-    float4() : x(0), y(0), z(0), w(0) {}
-    float4(float a, float b, float c, float d) : x(a), y(b), z(c), w(d) {}
-    explicit float4(float a[4]) : x(a[0]), y(a[1]), z(a[2]), w(a[3]) {}
+    inline float4() : x(0), y(0), z(0), w(0) {}
+    inline float4(float a, float b, float c, float d) : x(a), y(b), z(c), w(d) {}
+    inline explicit float4(float a[4]) : x(a[0]), y(a[1]), z(a[2]), w(a[3]) {}
 
-    float4(cvex::vfloat4 rhs) { v = rhs; }
-    float4 operator=(cvex::vfloat4 rhs) { v = rhs; return *this; }
+    inline float4(cvex::vfloat4 rhs) { v = rhs; }
+    inline float4 operator=(cvex::vfloat4 rhs) { v = rhs; return *this; }
     inline operator cvex::vfloat4() const { return v; }
     
-    float& operator[](int i)       { return v[i]; }
-    float  operator[](int i) const { return v[i]; }
-
-    inline float4 operator+(const float4& rhs) const { return v + rhs.v; }
-    inline float4 operator-(const float4& rhs) const { return v - rhs.v; }
-    inline float4 operator*(const float4& rhs) const { return v * rhs.v; }
-    inline float4 operator/(const float4& rhs) const { return v / rhs.v; }
-
-    inline float4 operator+(const float rhs) const { return v + rhs; }
-    inline float4 operator-(const float rhs) const { return v - rhs; }
-    inline float4 operator*(const float rhs) const { return v * rhs; }
-    inline float4 operator/(const float rhs) const { return v / rhs; }
+    inline float& operator[](int i)       { return v[i]; }
+    inline float  operator[](int i) const { return v[i]; }
 
     union
     {
@@ -58,6 +48,16 @@ namespace cmath
       cvex::vfloat4 v;
     };
   };
+
+  inline float4 operator+(const float4& a, const float4& b) { return a.v + b.v; }
+  inline float4 operator-(const float4& a, const float4& b) { return a.v - b.v; }
+  inline float4 operator*(const float4& a, const float4& b) { return a.v * b.v; }
+  inline float4 operator/(const float4& a, const float4& b) { return a.v / b.v; }
+
+  inline float4 operator+(const float4& a, const float rhs) { return a.v + rhs; }
+  inline float4 operator-(const float4& a, const float rhs) { return a.v - rhs; }
+  inline float4 operator*(const float4& a, const float rhs) { return a.v * rhs; }
+  inline float4 operator/(const float4& a, const float rhs) { return a.v / rhs; }
 
   inline float4 operator+(const float c, float4 v) { return c + v.v; }
   inline float4 operator-(const float c, float4 v) { return c - v.v; }
@@ -72,7 +72,5 @@ namespace cmath
   inline cvex::vint4 operator!=(const float4& a, const float4& b) { return (a.v != b.v); }
 
 };
-
-
 
 #endif
